08ee36342c6f418eebc612adeb826f2e9c07ba3f
Pizzafuglen <Crose19@student.sdu.dk>
2024-05-15 22:39:32 +0200

    Changes to game component

--- src/app/game-price/game-price.component.ts
+++ src/app/game-price/game-price.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, computed, effect, signal } from '@angular/core';
 import { Game, GamePriceService } from './game-price.service';
-import {MatButtonModule} from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import _ from 'lodash';
 import { AgGridAngular } from 'ag-grid-angular';
-import { GridOptions, ColDef } from 'ag-grid-community';
+import { GridOptions, ColDef, GridApi } from 'ag-grid-community';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -14,96 +16,95 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     MatButtonModule,
     AgGridAngular,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './game-price.component.html',
   styleUrl: './game-price.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GamePriceComponent implements OnInit {
-  public cheapestGames = computed<Game[]>(() => _.chunk(this.games().sort((a, b) => a.salePrice - b.salePrice), 3)[1]);
   public games = signal<Game[]>([])
-
+  private api: GridApi = new GridApi;
   public selectionForm: FormGroup = this.fb.group({
     noGames: [0]
   })
-
-  public columnDefs: ColDef[] = [
-        {
-          field: 'internalName',
-        },
-        {
-          field: 'title',
-        },
-        {
-          field: 'metacriticLink',
-        },
-        {
-          field: 'dealID',
-        },
-        {
-          field: 'storeID',
-        },
-        {
-          field: 'gameID',
-        },
-        {
-          field: 'salePrice',
-        },
-        {
-          field: 'normalPrice',
-        },
-        {
-          field: 'isOnSale',
-        },
-        {
-          field: 'savings',
-        },
-        {
-          field: 'metacriticScore',
-        },
-        {
-          field: 'steamRatingText',
-        },
-        {
-          field: 'steamRatingPercent',
-        },
-        {
-          field: 'steamRatingCount',
-        },
-        {
-          field: 'steamAppID',
-        },
-        {
-          field: 'releaseDate',
-        },
-        {
-          field: 'lastChange',
-        },
-        {
-          field: 'dealRating',
-        },
-        {
-          field: 'thumb',
-        }
-      ]
-  // TODO: Use GridOptions instead
+  public cheapestGames = computed<Game[]>(() => _.chunk(this.games().sort((a, b) => a.salePrice - b.salePrice), this.selectionForm.get('noGames')?.value)[1] ?? []);
 
   public gridOptions: GridOptions = {
-    rowData: this.cheapestGames(),
-    columnDefs: this.columnDefs,
-    onGridReady: () => this.load(),
+    rowData: [],
+    columnDefs: [
+      {
+        field: 'internalName',
+      },
+      {
+        field: 'title',
+      },
+      {
+        field: 'metacriticLink',
+      },
+      {
+        field: 'dealID',
+      },
+      {
+        field: 'storeID',
+      },
+      {
+        field: 'gameID',
+      },
+      {
+        field: 'salePrice',
+      },
+      {
+        field: 'normalPrice',
+      },
+      {
+        field: 'isOnSale',
+      },
+      {
+        field: 'savings',
+      },
+      {
+        field: 'metacriticScore',
+      },
+      {
+        field: 'steamRatingText',
+      },
+      {
+        field: 'steamRatingPercent',
+      },
+      {
+        field: 'steamRatingCount',
+      },
+      {
+        field: 'steamAppID',
+      },
+      {
+        field: 'releaseDate',
+      },
+      {
+        field: 'lastChange',
+      },
+      {
+        field: 'dealRating',
+      },
+      {
+        field: 'thumb',
+      }
+    ],
+    onGridReady: (param) => this.api = param.api,
   }
 
   constructor(
     private gameService: GamePriceService,
     private fb: FormBuilder
   ) {
-    // effect(() => {console.log(this.games() ? this.games() : ''); console.log(this.cheapestGames() ? this.cheapestGames() : ''); console.log(this.cheapestGames() ? typeof(this.cheapestGames()[1]?.salePrice): '')})
+    effect(() => { this.api.setGridOption('rowData', []) })
   }
 
   ngOnInit(): void {
-    // this.load()
+    this.load()
   }
 
   public load(): void {
