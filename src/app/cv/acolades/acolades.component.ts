7317509c529ddb969529a688f43436535b3ba5ac
Pizzafuglen <Crose19@student.sdu.dk>
2024-07-13 02:43:20 +0200

    Changes to overall, and acolade selector

--- src/app/cv/acolades/acolades.component.ts
+++ src/app/cv/acolades/acolades.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
 import { CarouselComponent } from '../../fundamentals/carousel/carousel.component';
+interface Acolade {
+  title: string,
+  duration: string,
+  description: string,
+  tags: string[],
+}
 
 @Component({
     selector: 'app-acolades',
@@ -14,28 +20,37 @@ import { CarouselComponent } from '../../fundamentals/carousel/carousel.componen
     ]
 })
 export class AcoladesComponent implements OnInit {
-  public testAcolades = [
+  public testAcolades: Acolade[] = [
     {
      title: 'Test',
      duration: '2014-2015',
-     description: 'This is a test description for my acolades slider'
+     description: 'This is a test description for my acolades slider',
+     tags: ['Tag1', 'Tag2', 'Tag3'],
     },
     {
       title: 'Test1',
       duration: '2014-2015',
-      description: 'This is a test description for my acolades slider'
+      description: 'This is a test description for my acolades slider',
+      tags: ['Tag1', 'Tag2', 'Tag3'],
      },
      {
       title: 'Test2',
       duration: '2014-2015',
-      description: 'This is a test description for my acolades slider'
+      description: 'This is a test description for my acolades slider',
+      tags: ['Tag1', 'Tag2', 'Tag3'],
      },
      {
       title: 'Test3',
       duration: '2014-2015',
-      description: 'This is a test description for my acolades slider'
+      description: 'This is a test description for my acolades slider',
+      tags: ['Tag1', 'Tag2', 'Tag3'],
      },
   ]
-  ngOnInit(): void { }
 
+  public currentAcolade: Acolade | null = null;
+
+  public selectCurrentAcolade(): Acolade {
+    return this.testAcolades[1]
+  }
+  ngOnInit(): void { }
 }
